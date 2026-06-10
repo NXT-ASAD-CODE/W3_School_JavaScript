c88580e62abe931aac478e66f917690b5105fe37
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-10 15:00:49 +0500

    Styling text using JavaScript and changing the source of an image using JavaScript

---
+++ 002 JavaScript Introduction/index.js
@@ -0,0 +1,18 @@
+// JavaScript Can Change HTML Content
+document.getElementById("new").innerHTML = "Hello JavaScript";
+
+
+// JavaScript Can Change HTML Attribute Values
+// bulb example in html
+
+
+// JavaScript Can Change HTML Styles (CSS)
+document.getElementById('new').style.fontSize ="35px"
+
+
+// JavaScript Can Hide HTML Elements
+document.getElementById("new").style.display = "none";
+
+
+// JavaScript Can Show HTML Elements
+document.getElementById("new").style.display = "block";
