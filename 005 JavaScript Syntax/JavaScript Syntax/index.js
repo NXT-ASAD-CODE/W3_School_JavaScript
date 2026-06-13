8bbf1c514323177978bd249a2231eaa22adca64b
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-13 14:44:10 +0500

    JavaScript variables let and const. Identifiers , camelCase ,Keywords are Case sensitive. Explaning syntax rules.

---
+++ 005 JavaScript Syntax/JavaScript Syntax/index.js
@@ -0,0 +1,68 @@
+// Syntax Rules
+
+// How to declare variables
+let a = 10;
+let b = 20;
+
+// Computing Values
+let c = a + b ;
+console.log(a+b);
+
+// JavaScript Values are of two types
+// Literals (Fixed Values)
+// Varaibles (Changeable Values )
+
+
+// Literals 
+
+document.getElementById("decimal").innerText = 10.50 // only 10.5 will show on screen
+
+// "Asad" and 'Asad' are same 
+
+// let and const
+let d = 5;
+const myname = "Asad"; // Keywords are case sensitive
+
+// JavaScript Variables
+let x;
+x = 6;
+
+// JavaScript Identifiers
+
+// Must start with a letter, _ , or $
+// Can contain digits after the first character
+// Cannot be a reserved keyword (let, const, if, etc.)
+// Are case-sensitive
+
+// JavaScript Operators
+
+let x = 5;
+let y = 6;
+let sum = x + y;
+let subtract = x - y;
+let divide = x / y;
+let multiply = x * y;
+
+// JavaScript Expressions
+
+// Example 1
+ f = (5+6)*10;
+
+ // Example 2
+ z = a*10;
+
+ // Example 2
+ fullName = "Asad" + " " + "Iftikhar";
+
+ // JavaScript is Case Sensitive
+
+//  myname and myName are different
+
+let myName = "Asad";
+let myname = "asad";
+
+// JavaScript programmers tend to use lower camel case.
+
+// Example are :- firstName
+
+
