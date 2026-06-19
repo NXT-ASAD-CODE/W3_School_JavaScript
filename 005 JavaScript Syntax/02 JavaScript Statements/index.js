8ba6d87551cb99acd75f3bbc190b9ddc074fad35
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-19 21:00:07 +0500

    JavaScript Statements , Semicolon (;) , White Spaces and much more

---
+++ 005 JavaScript Syntax/02 JavaScript Statements/index.js
@@ -0,0 +1,22 @@
+a = 10;
+b = 20;
+c = a + b;
+
+
+document.getElementById("test").innerText = "The value from the index.js file is " + c
+document.getElementById("one").innerText = "Hello World"; // semicolon (;) are optional in JavaScript but in some places they are strictly required;
+let uname =  "Asad";
+let myname ="asad" // JavaScript ignores multiple spaces
+
+// This is a block of code
+function myFunction() {
+      document.getElementById("test").innerHTML = "Hello Asad!";
+    document.getElementById("one").innerHTML = "How are you?";
+}
+// there are some defined keywords is JavaScript
+// like 
+var a ;
+let b ;
+const c ;
+if (a);
+
