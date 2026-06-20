bd22d3206e63775fc2dee4138d63cc0f770c4236
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-20 12:58:22 +0500

    Single and Multi Line Commnets in JavaScript

---
+++ 005 JavaScript Syntax/03 JavaScript Comments/index.js
@@ -0,0 +1,14 @@
+// There are two types of comments in JavaScript
+
+console.log("Conments in JavaScript");
+document.getElementById("heading").innerText = "Comments in JavaScript";
+document.getElementById("test").innerText = "There are Two types of Comments ";
+
+// Single Line Comments
+// they are start  with " // "
+document.getElementById("multi").innerText = "They are written with // in JavaScript";
+// Multi Line Comments 
+document.getElementById("single").innerText = "They are written with /* Some Text here */ in JavaScript";
+// They are start with /* Some text  here */
+
+
