81fd5d59139239d434df26e330bdc138eb9495a5
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-06-23 11:58:20 +0500

    JavaScript Variables : Delearing , Rules for naming Variables ,  let vs const Difference

---
+++ 005 JavaScript Syntax/04 JavaScript Variables/index.js
@@ -0,0 +1,57 @@
+// JavaScript Variables
+
+// Example of let variable
+
+let a = 10;
+let b = 10;
+let c = a + b;
+
+// Example of const variable
+
+const d = 10;
+const e = 10;
+const f = d + e;
+console.log(f);
+
+// Rules for naming Variables
+
+// Names can contain letters, digits, underscores, and dollar signs.
+// Names must begin with a letter, a $ sign or an underscore (_).
+// Names are case sensitive (X is different from x).
+// Reserved words (JavaScript keywords) cannot be used as names.
+
+// JavaScript Underscore (_)
+let _myName = "Asad";
+
+// JavaScript Dollar Sign $
+let $ = "20 dollar";
+
+// Declaring a Variable Using let
+let firstName;
+
+// Assigning a Variable 
+firstName = "Asad"
+
+// Declaring a Variable Using Const
+const uname = "Ali"
+
+// Declaring a Variable Automatically (Not Recommended)
+g = 10;
+
+// Declaring a Variable Using var
+var h = 20;
+
+// JavaScript Data Types
+
+const pi = 3.14;
+let person = "Asad";
+
+// One Statement, Many Variables
+let job = "WebDevelopment" , className = "Intermediate" , pay = "80k"
+
+// The Assignment Operator
+i = a + b
+
+// JavaScript Arithmetic 
+let j = 2 + 3 + 5
+
