213be17671382b96ae807226350772489b25342d
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-07-19 20:47:24 +0500

    JavaScript Constant , Block Scope , Redeclaring

--- 005 JavaScript Syntax/06 JavaScript Const/index.js
+++ 005 JavaScript Syntax/06 JavaScript Const/index.js
@@ -53,3 +53,33 @@ const x = 2;
 var x = 2;     // Allowed
 var x = 3;     // Allowed
 x = 4;         // Allowed
+
+// Redeclaring an existing var or let variable to const, in the same scope, is not allowed:
+
+const x = 2;     // Allowed
+x = 2;           // Not allowed
+var x = 2;       // Not allowed
+let x = 2;       // Not allowed
+const x = 2;     // Not allowed
+
+{
+  const x = 2;   // Allowed
+  x = 2;         // Not allowed
+  var x = 2;     // Not allowed
+  let x = 2;     // Not allowed
+  const x = 2;   // Not allowed
+}
+
+// Redeclaring a variable with const, in another scope, or in another block, is allowed:
+
+const x = 2;       // Allowed
+
+{
+  const x = 3;   // Allowed
+}
+
+{
+  const x = 4;   // Allowed
+}
+
+
