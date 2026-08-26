99436faeb9642d3d1930c498da5ab566d335ee3b
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-26 20:12:20 +0500

    Adding More Examples of Typeof Operator

--- 005 JavaScript Syntax/07 JavaScript Types/index.js
+++ 005 JavaScript Syntax/07 JavaScript Types/index.js
@@ -44,3 +44,9 @@ let f = Symbol()
 typeof ""             // Returns "string"
 typeof "John"         // Returns "string"
 typeof "John Doe"     // Returns "string"
+
+typeof 0              // Returns "number"
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof (3)            // Returns "number"
+typeof (3 + 4)        // Returns "number"
