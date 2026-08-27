d7afe77449c4bbc27bcd969050c404a61f292c9e
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-27 20:34:50 +0500

    JavaScript Assignment Operators , Logical Operators , Comparision Operator

--- 006 JavaScript Operators/01 JavaScript Operators/index.js
+++ 006 JavaScript Operators/01 JavaScript Operators/index.js
@@ -53,3 +53,45 @@ let text3 = text1 + " " + text2;
 // Example
 let text4 = "What a very ";
 text4 += "nice day";
+
+// Adding Strings and Numbers
+// Adding two numbers, will return the sum as a number like 5 + 5 = 10.
+// Adding a number and a string, will return the sum as a concatenated string like 5 + "5" = "55".
+// Example
+let h = 5 + 5; // 10
+let i = "5" + 5; // 55
+let j = "Hello" + 5; // Hello5
+
+// JavaScript Assignment Operators
+// Assignment operators assign values to JavaScript variables.
+// The Addition Assignment Operator (+=) adds a value to a variable.
+// Example
+let k = 10;
+k += 5; // k = 15
+
+// JavaScript Comparison Operators
+// Comparison operators are used to compare two values.
+// Comparison operators always return true or false.
+// Example 
+let l = 5;
+let result = l > 8; // false
+
+// Operator	Description	Example
+// ==	equal to	x == 5
+// ===	equal value and equal type	x === 5
+// !=	not equal	x != 5
+// !==	not equal value or not equal type	x !== 5
+// >	greater than	x > 5
+// <	less than	x < 5
+// >=	greater than or equal to	x >= 5
+// <=	less than or equal to	x <= 5
+
+// Example
+let text5 = "A";
+let text6 = "B";
+let result2 = text1 < text2;
+
+// JavaScript Logical Operators
+// &&	logical and
+// ||	logical or
+// !	logical not
