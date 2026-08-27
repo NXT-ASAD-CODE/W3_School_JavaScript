c33323e0d907dc8e98bdc552ee36d61582755d70
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-27 19:56:05 +0500

    Operaters in JavaScript

--- 006 JavaScript Operators/01 JavaScript Operators/index.js
+++ 006 JavaScript Operators/01 JavaScript Operators/index.js
@@ -1,1 +1,10 @@
 // JavaScript Operators
+
+// Operators are for Mathematical and Logical Computations
+// The Assignment Operator = assigns values
+
+// The Addition Operator + adds values
+
+// The Multiplication Operator * multiplies values
+
+// The Comparison Operator > compares values
