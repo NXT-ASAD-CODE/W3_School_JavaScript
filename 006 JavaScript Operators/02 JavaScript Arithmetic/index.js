264b2d618d733f1e3197ceca89516521416adcea
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-08-28 21:12:16 +0500

    Arithmetic Operations

--- 006 JavaScript Operators/02 JavaScript Arithmetic/index.js
+++ 006 JavaScript Operators/02 JavaScript Arithmetic/index.js
@@ -1,1 +1,24 @@
 // JavaScript Arithmetic
+
+// JavaScript Arithmetic Operators
+
+// Operator	Description
+//   +	    Addition
+//   -	    Subtraction
+//   *	    Multiplication
+//   **	    Exponentiation (ES2016)
+//   /	    Division
+//   %	    Modulus (Remainder)
+//   ++	    Increment
+//   --	    Decrement
+
+// Arithmetic Operations
+
+// Example 1
+let x = 100 + 50;
+
+// Example 2
+let c = a + b;
+
+// Example 3
+let d = (100 + 50) * a;
