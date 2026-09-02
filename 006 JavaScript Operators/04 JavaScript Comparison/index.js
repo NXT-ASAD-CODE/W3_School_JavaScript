a8d71ce6eade8caffad0ec6fe32104843f854e02
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-02 20:55:09 +0500

    JavaScript String Comparison

--- 006 JavaScript Operators/04 JavaScript Comparison/index.js
+++ 006 JavaScript Operators/04 JavaScript Comparison/index.js
@@ -22,3 +22,17 @@
 
 // Comparison operators can be used in conditional statements to compare values and take action depending on the result:
 if (age < 18) text = "Too young for the car linecce";
+
+
+// JavaScript String Comparison
+
+// Example 1
+let text1 = "A";
+let text2 = "B";
+let result = text1 < text2; // true
+
+
+// Example 2
+let text3 = "20";
+let text4 = "5";
+let result1 = text1 < text2; // still true it will see the 2 not the 0
