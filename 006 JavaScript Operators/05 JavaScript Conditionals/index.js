740754b4ddf5c7ee0beb7225db8824753f06effc
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-03 21:29:36 +0500

    When to use Conditionals

---
+++ 006 JavaScript Operators/05 JavaScript Conditionals/index.js
@@ -0,0 +1,16 @@
+// Conditional Statements
+// Conditional Statements allow us to perform different actions for different conditions.
+// Conditional statements run different code depending on true or false conditions.
+// Conditional statements include:
+// if
+// if...else
+// if...else if...else
+// switch
+// ternary (? :)
+
+// When to use Conditionals
+// Use if to specify a code block to be executed, if a specified condition is true
+// Use else to specify a code block to be executed, if the same condition is false
+// Use else if to specify a new condition to test, if the first condition is false
+// Use switch to specify many alternative code blocks to be executed
+// Use (? :) (ternary) as a shorthand for if...else
