75a713e9a5e1ee9060d01239b820facfa81f2b1d
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-09 22:14:06 +0500

    Adding the 1 Example of the If Condtion

--- 007 JavaScript if Conditions/01 JavaScript if/index.js
+++ 007 JavaScript if Conditions/01 JavaScript if/index.js
@@ -6,3 +6,11 @@
 if (condition) {
   //  block of code to be executed if the condition is true
 }
+
+
+// Note that if is in lowercase letters. Uppercase letters (If or IF) will generate a JavaScript erro
+
+// Example 1
+if (hour < 18) {
+  greeting = "Good day";
+}
