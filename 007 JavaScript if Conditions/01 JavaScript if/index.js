8def92c4a705867b7c888f7c8641f02025c63e87
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-09 22:11:14 +0500

    The JavaScript if Statement

--- 007 JavaScript if Conditions/01 JavaScript if/index.js
+++ 007 JavaScript if Conditions/01 JavaScript if/index.js
@@ -1,1 +1,8 @@
 // JavaScript if
+
+// The JavaScript if Statement
+// Use the JavaScript if statement to execute a block of code when a condition is true.
+// Syntax
+if (condition) {
+  //  block of code to be executed if the condition is true
+}
