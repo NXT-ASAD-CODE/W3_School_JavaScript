39b58619186c0b5d28d5742750d77570cad0ec8d
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-14 21:58:47 +0500

    The else if statement

--- 007 JavaScript if Conditions/02 JavaScript if else/index.js
+++ 007 JavaScript if Conditions/02 JavaScript if else/index.js
@@ -7,3 +7,22 @@ if (condition) {
 } else {
   //  block of code to be executed if the condition is false
 }
+
+// Example
+// If the hour is less than 18, create a "Good day" greeting, otherwise "Good evening"
+if (hour < 18) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+// The else if Statement
+// Use the else if statement to specify a new condition if the first is false.
+// Syntax
+if (condition1) {
+  //  block of code to be executed if condition1 is true
+} else if (condition2) {
+  //  block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  //  block of code to be executed if the condition1 is false and condition2 is false
+}
