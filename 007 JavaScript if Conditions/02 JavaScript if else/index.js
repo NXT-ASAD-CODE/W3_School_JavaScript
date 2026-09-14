3882c814445dd152fbff536d81292a3248f43bba
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-14 21:53:08 +0500

    The else Statement

--- 007 JavaScript if Conditions/02 JavaScript if else/index.js
+++ 007 JavaScript if Conditions/02 JavaScript if else/index.js
@@ -1,2 +1,9 @@
 // JavaScript else
 // The else Statement
+
+// Use the else statement to specify a block of code to be executed if a condition is false.
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
