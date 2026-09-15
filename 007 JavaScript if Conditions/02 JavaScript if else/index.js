c0fdd885d08df2d9ef3db8d7567132c2a3ca90f0
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-15 21:20:52 +0500

    Example of The else if Statement

--- 007 JavaScript if Conditions/02 JavaScript if else/index.js
+++ 007 JavaScript if Conditions/02 JavaScript if else/index.js
@@ -26,3 +26,14 @@ if (condition1) {
 } else {
   //  block of code to be executed if the condition1 is false and condition2 is false
 }
+
+// Example  
+// If time is less than 10:00, create a "Good morning" greeting, if not, but time is less than 20:00, create a "Good day" greeting, otherwise a "Good evening":
+
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
