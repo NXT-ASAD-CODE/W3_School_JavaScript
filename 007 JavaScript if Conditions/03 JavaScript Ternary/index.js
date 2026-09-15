5b73396d85496b7939c846f89ae4e963009e0222
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-15 21:52:12 +0500

    Description of Ternary Operator

--- 007 JavaScript if Conditions/03 JavaScript Ternary/index.js
+++ 007 JavaScript if Conditions/03 JavaScript Ternary/index.js
@@ -7,3 +7,10 @@ let text = (age < 18) ? "You cannot drive" : "You can drive";
 // Example 2
 let isMember = true;
 let discount = isMember ? 0.2 : 0;
+
+// Description
+// The conditional operator is a shorthand for writing conditional if...else statements.
+// It is called a ternary operator because it takes three operands.
+
+// Syntax
+(condition) ? expression1 : expression2
