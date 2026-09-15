d0029d06c67a44d2cbd9d47204d49933a7287337
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-15 21:44:06 +0500

    Example of Ternary Operator

--- 007 JavaScript if Conditions/03 JavaScript Ternary/index.js
+++ 007 JavaScript if Conditions/03 JavaScript Ternary/index.js
@@ -1,1 +1,5 @@
 // JavaScript Ternary Operator
+
+// Example 1
+// If the value of age is < 18, set the value of text to "You cannot drive", otherwise to "You can drive":
+let text = (age < 18) ? "You cannot drive" : "You can drive";
