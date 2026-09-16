7a971b2381c989983296a767e1f9914ab26c08ca
NXT-ASAD-CODE <asadmulti786@gmail.com>
2026-09-16 20:10:11 +0500

    Switch Control Flow

--- 007 JavaScript if Conditions/04 JavaScript Switch/index.js
+++ 007 JavaScript if Conditions/04 JavaScript Switch/index.js
@@ -1,1 +1,8 @@
 // JavaScript Switch Statement
+
+// Switch Control Flow
+// Based on a condition, switch selects one or more code blocks to be executed.
+
+// switch executes the code blocks that matches an expression.
+
+// switch is often used as a more readable alternative to many if...else if...else statements, especially when dealing with multiple possible values.
